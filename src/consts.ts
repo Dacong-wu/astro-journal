3ecf3439d2143b585fac529c7cce140c6eabafef
大葱 <2420390517@qq.com>
2024-05-14 23:18:39 +0800

    更新Title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SiteTitle = 'Starlog';
-export const SiteDescription = 'Welcome to my website!';
+export const SiteTitle = '幸福日志';
+export const SiteDescription = '欢迎来到幸福日志，由以后的路一起走记录制作';
