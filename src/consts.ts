52494bd0d1711883a525dce5cd578745cb2c0d2e
大葱 <2420390517@qq.com>
2024-05-19 15:45:34 +0800

    add content

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SiteTitle = '🎉幸福日志'
-export const SiteDescription = '欢迎来到幸福日志，由以后的路一起走记录制作'
+export const SiteDescription = '幸福日志，记录幸福生活，记录、分享、成长。'
