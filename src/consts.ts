24f9fad8048a34e8bfe2f3bf8b9e29c5b0d5ebf7
大葱 <2420390517@qq.com>
2024-07-31 21:42:26 +0800

    update: set new title

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SiteTitle = '🎉幸福日志'
-export const SiteDescription = '幸福的事情更要记录'
+export const SiteTitle = '🎉每年的今天'
+export const SiteDescription = '每年的今天，幸福瞬间要记录'
 export const SiteImage = {
   src: 'https://2018-0215.cn/apple-touch-icon.png',
   alt: 'site-image',
